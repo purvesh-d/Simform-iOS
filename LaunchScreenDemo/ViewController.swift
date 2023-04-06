d84c1dda6660d5c2db6e3067ef2d67409e5ef916
vidhi48 <pvidhi0408@gmail.com>
2023-04-06 19:09:07 +0530

    TE7-T730 Demo size screen completed.

--- LaunchScreenDemo/ViewController.swift
+++ LaunchScreenDemo/ViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonCorners: UIButton!
+    @IBOutlet weak var BorderDemo: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        BorderDemo.layer.borderWidth = 1
+        BorderDemo.layer.borderColor = UIColor.gray.cgColor
+        BorderDemo.layer.cornerRadius = 5
         
+        buttonCorners.layer.cornerRadius = 23
     }
     
 }
