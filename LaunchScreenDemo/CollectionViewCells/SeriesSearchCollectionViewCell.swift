afe8483b723c2d93569756d01aaf31ca47988e2e
vidhi48 <pvidhi0408@gmail.com>
2023-04-26 17:06:59 +0530

    TE7-T730 UIComponents Completed

---
+++ LaunchScreenDemo/CollectionViewCells/SeriesSearchCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SeriesSearchCollectionViewCell.swift
+//  LaunchScreenDemo
+//
+//  Created by Vidhi Patel on 26/04/23.
+//
+
+import UIKit
+
+class SeriesSearchCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var seriesName: UILabel!
+}
