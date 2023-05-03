cc301af2d7266f0486d9e40c815468f30ad5df23
vidhi48 <pvidhi0408@gmail.com>
2023-05-03 16:32:34 +0530

    UIComponents done

--- LaunchScreenDemo/CollectionViewCells/HomeCollectionViewCell.swift
+++ LaunchScreenDemo/CollectionViewCells/HomeCollectionViewCell.swift
@@ -9,14 +9,5 @@ import UIKit
 
 class HomeCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet private weak var subtitleLbl: UILabel!
-    @IBOutlet private weak var titleLabel: UILabel!
-    @IBOutlet private weak var imageView: UIImageView!
-    
-    func configCell(data: HomeModel) {
-        layer.cornerRadius = 10
-        imageView.image = UIImage(named: data.imageName)
-        titleLabel.text = data.title
-        subtitleLbl.text = data.subtitle
-    }
+    @IBOutlet weak var imageView: UIImageView!
 }
