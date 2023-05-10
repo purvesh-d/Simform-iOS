dbf68471adb5e41cf684adfb2025a6519c105c9a
vidhi48 <pvidhi0408@gmail.com>
2023-05-10 11:31:44 +0530

    UINavigationController started.

---
+++ LaunchScreenDemo/NavigationController/ViewControllers/FirstViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.swift
+//  LaunchScreenDemo
+//
+//  Created by Vidhi Patel on 10/05/23.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ LaunchScreenDemo/NavigationController/ViewControllers/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  LaunchScreenDemo
+//
+//  Created by Vidhi Patel on 10/05/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
